3e6d8210b9f944c63ed29932a0413e526baa2a02
Benjamin Yates <benjamin@rqdq.com>
2015-10-02 22:28:48 -0400

    fix install dep order

--- build-all.sh
+++ build-all.sh
@@ -10,7 +10,7 @@ wget -c https://bootstrap.pypa.io/get-pip.py
 echo 'bootstrap setuptools:'
 sudo /opt/rqdq/bin/python2.7 ./get-pip.py
 
-for pkg in libxml2 libxslt mod_wsgi python2-pip python2-setuptools python2-virtualenv
+for pkg in libxml2 libxslt mod_wsgi python2-setuptools python2-pip python2-virtualenv
 do
 	pushd $pkg
 	./build.sh
